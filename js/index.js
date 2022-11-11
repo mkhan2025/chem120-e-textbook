caa23894382cf348f775340eefc74e07a26d83ca
ado_ubuntu <anhphuong.do34@gmail.com>
2022-11-10 20:20:03 -0500

    replace check alert with icons

--- js/index.js
+++ js/index.js
@@ -14,10 +14,6 @@ const incorrectDeduct = 10;
 // Game constants
 // const endPoint = "http://127.0.0.1:5000/";
 const endPoint = "https://chem120-game.up.railway.app/";
-const correctMessage = "Correct answer";
-const wrongMessage = "Incorrect answer";
-const dupMessage = "Duplicated answer";
-const levelIncomplete = "You haven't found all isomers. Keep going!";
 
 // Interaction state
 let pointerIsDown = false;
@@ -58,6 +54,12 @@ const MENU_NEXT = Symbol("MENU_NEXT");
 const MENU_TUTORIAL_PAUSE = Symbol("MENU_TUTORIAL_PAUSE");
 const MENU_TUTORIAL_MAIN = Symbol("MENU_TUTORIAL_MAIN");
 
+// Answer Check
+const CORRECT = "Correct!";
+const INCORRECT = "Incorrect!";
+const DUPLICATED = "Duplicated!";
+const UNFINISHED = "Incomplete.\nKeep trying!";
+
 ///////////
 // Icons //
 ///////////
@@ -65,6 +67,7 @@ const soundOn =
 	"<svg xmlns='http://www.w3.org/2000/svg' width='48' height='48' fill='white' viewBox='0 0 16 16'><path d='M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z'/><path d='M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z'/><path d='M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z'/></svg>";
 const soundOff =
 	"<svg xmlns='http://www.w3.org/2000/svg' width='48' height='48' fill='white' class='bi bi-volume-mute-fill' viewBox='0 0 16 16'><path d='M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zm7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z'/></svg>";
+
 
 //////////////////
 // Global State //
@@ -150,9 +153,7 @@ const handlePointerDown = (element, handler) => {
 // Converts a number into a formatted string with thousand separators.
 const formatNumber = (num) => num.toLocaleString();
 
-////////////////////
-// Timing Helpers //
-////////////////////
+
 
 // hud.js
 // ============================================================================
@@ -236,6 +237,27 @@ function renderScoreHud() {
 		state.game.totalScore + state.game.lvlScore
 	}`;
 }
+
+///////////
+// Check //
+///////////
+const wrongIcon = $("#icon-wrong");
+const correctIcon = $("#icon-correct");
+const duplicateIcon = $("#icon-duplicate");
+const unfinishIcon = $("#icon-unfinish");
+const checkMessageNode = $(".alert");
+
+const getMolAlert = (iconNode, alertMessage) => {
+	iconNode.classList.add("active");
+	checkMessageNode.innerText = alertMessage;
+	checkMessageNode.classList.add("active");
+
+	setTimeout(() => {
+		iconNode.classList.remove("active");
+		checkMessageNode.classList.remove("active");
+	}, 1500);
+};
+
 
 ///////////
 // Sound //
@@ -343,6 +365,7 @@ const startLvl = () => {
 // Sound Control
 const soundBtn = $(".sound-control");
 const bgMusicNode = $("#bg-music");
+bgMusicNode.volume = 0.2;
 handleClick(soundBtn, () => {
 	state.sound = !state.sound;
 	bgMusicNode.muted = !bgMusicNode.muted;
@@ -582,13 +605,13 @@ const checkOneMol = async () => {
 					changeScore(levels[state.game.level].molScore);
 					displayCorrectAns(data["molBlock"]);
 					clearCanvas();
-					alert(correctMessage);
+					getMolAlert(correctIcon, CORRECT);
 				} else if (!notDup) {
 					changeScore(-dupDeduct);
-					alert(dupMessage);
+					getMolAlert(duplicateIcon, DUPLICATED);
 				} else {
 					changeScore(-incorrectDeduct);
-					alert(wrongMessage);
+					getMolAlert(wrongIcon, INCORRECT);
 				}
 			});
 		})
@@ -635,7 +658,7 @@ const checkMolAndLvl = async () => {
 					} else {
 						endLevel();
 					}
-				} else alert(levelIncomplete);
+				} else getMolAlert(unfinishIcon, UNFINISHED);
 			});
 		})
 		.catch((e) => {
